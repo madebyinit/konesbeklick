df4b4c7f6b4225c37906c37bf79cd950973b4f6e
Theodore Dominiak <ted.dominiak@gmail.com>
2020-01-26 20:33:57 +0200

    updated the homepage countdown widget logic

--- inc/ajax/next-closing-auction.php
+++ inc/ajax/next-closing-auction.php
@@ -2,41 +2,45 @@
 
     function next_closing_auction_countdown($is_ajax = true) {
         
-        $query_args = array('posts_per_page' => -1, 'no_found_rows' => 1, 'post_status' => 'publish', 'post_type' => 'product');
-		$query_args['tax_query'] = array(array('taxonomy' => 'product_type' , 'field' => 'slug', 'terms' => 'auction')); 
-		$query_args['auction_arhive'] = true;
+        $query_args = array('posts_per_page' => 1, 'post_status' => 'publish', 'post_type' => 'product');
 		$query_args['meta_key'] = 'woo_ua_auction_end_date';
 		$query_args['orderby']  = 'meta_value';
-		$query_args['order']    = 'DESC';		
+		$query_args['order']    = 'ASC';
+		$query_args['meta_query'] = array(
+		    array(
+                'key' => 'woo_ua_auction_end_date',
+                'value' => date('Y-m-d H:i:s'),
+                'compare' => '>=',
+                'type' => 'DATETIME'
+            )
+        );
 
         $uwa_query = new WP_Query($query_args);
-        $count_to = '';
 
-        foreach($uwa_query->posts as $auction) {
-            $oldTime = strtotime(get_post_meta($auction->ID ,'woo_ua_auction_end_date', true)); 
+        $end_time = strtotime(get_post_meta($uwa_query->posts[0]->ID ,'woo_ua_auction_end_date', true));
+        $raw_time = $end_time - strtotime("now");
+        $end_days = $raw_time / 60 / 60 / 24;
+        $end_hours = ($end_days - intval($end_days)) * 24;
+        $end_minutes = ($end_hours - intval($end_hours)) * 60;
+        $end_sec = ($end_minutes - intval($end_minutes)) * 60;
+        $end_hours = ($end_hours - 2) > 0 ? $end_hours - 2 : 0;
 
-            if($oldTime > time()) {
-                $count_to = $oldTime;
-                break;
-            }
-        }
-
-        if(! empty($count_to)) {
+        if(! empty($end_time)) {
             
             if($is_ajax) {
 
                 die(json_encode(array(
-                    'hours' => date('H', $count_to),
-                    'minutes' => date('i', $count_to),
-                    'seconds' => date('s', $count_to)
+                    'hours'   => floor($end_days * 24),
+                    'minutes' => floor($end_minutes),
+                    'sec'     => floor($end_sec)
                 )));    
 
             } else {
 
                 return array(
-                    'hours' => date('H', $count_to),
-                    'minutes' => date('i', $count_to),
-                    'seconds' => date('s', $count_to)
+                    'hours'   => floor($end_days * 24),
+                    'minutes' => floor($end_minutes),
+                    'sec'     => floor($end_sec)
                 );
             }
         }
--- inc/shortcodes/homepage-timer.php
+++ inc/shortcodes/homepage-timer.php
@@ -2,6 +2,7 @@
 
     function homepage_timer_shortcode() {
         $times = next_closing_auction_countdown(false);
+
         return '<p style="text-align: center; direction: ltr;"><span class="hours">' . $times['hours'] . '</span> <strong><span style="color: #3fd77f;">:</span></strong> <span class="minutes">' . $times['minutes'] . '</span><strong> <span style="color: #3fd77f;">:</span></strong> <span class="seconds">' . $times['seconds'] . '</span></p>';
     }
     add_shortcode('homepage-timer', 'homepage_timer_shortcode');
