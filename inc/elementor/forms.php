12615e78d18a5808f10e1d76118d487b070f7ded
Theodore Dominiak <ted.dominiak@gmail.com>
2020-01-26 11:18:49 +0200

    added the ability to add time to an auction from the admin dashboard

--- inc/elementor/forms.php
+++ inc/elementor/forms.php
@@ -13,6 +13,12 @@ function product_dashboard_actions($record, $handler) {
         case 'update_increment':
             $amount = $raw_fields['amount']['value'];
             update_auction_bid_increment($amount, $post_id);
+            break;
+
+        case 'update_time':
+            $time = $raw_fields['time_addition']['value'];
+            add_time_to_auction($time, $post_id);
+            break;
 
         default:
             break;
@@ -36,7 +42,11 @@ function product_dashboard_listener() {
                         values = $.parseJSON(resp.data.output);
 
                     if(form_name == 'update_increment') {
-                        $('#bid-inc').text((parseInt($('#top-bid').html().replace(',', '')) + parseInt(values.amount.value)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+                        $('#bid-inc, #click-to-bid').text((parseInt($('#top-bid').html().replace(',', '')) + parseInt(values.amount.value)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+                    }
+
+                    else if(form_name == 'update_time') {
+                        window.location.reload();
                     }
                 });
             });            
@@ -50,3 +60,26 @@ function update_auction_bid_increment($amount, $post_id) {
     return update_post_meta($post_id, 'woo_ua_bid_increment', $amount);
 }
 
+function add_time_to_auction($time, $post_id) {
+    $current_end_date = get_post_meta($post_id, 'woo_ua_auction_end_date', true);
+
+    if($current_end_date && ! empty($current_end_date)) {
+
+        $end_date = strtotime($current_end_date);
+        $end_date_exploded = explode(':', $time);
+
+        $hours = $end_date_exploded[0];
+        $minutes = $end_date_exploded[1];
+        $decimal = convert_time($hours, $minutes);
+
+        $new_end_date = date("Y-m-d H:i:s", $end_date + ($decimal * 60));
+        
+        if($new_end_date) {
+            update_post_meta($post_id, 'woo_ua_auction_end_date', $new_end_date);
+        }
+    }
+}
+
+function convert_time($hours, $minutes) {
+    return $hours + round($minutes / 60, 2);
+}
